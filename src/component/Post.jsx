e1ef9d5b1cc49b3e5f2708098aed370bf8400379
jarkendia <harukendia@gmail.com>
2023-06-28 20:45:02 +0200

    Añadida una función sencilla para compartir la URL en el portapapeles y mensaje de éxito

--- src/component/Post.jsx
+++ src/component/Post.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { getPostByNameFromUserService } from "../services";
@@ -12,6 +13,35 @@ import bookmarkIcon from "../../logos/bookmark.svg";
 export const Post = ({ post }) => {
   const backendURL = import.meta.env.VITE_BACKEND;
   const imageName = post?.post_image?.split(".")[0];
+  const [isShareOpen, setIsShareOpen] = useState(false);
+  const [copySuccess, setCopySuccess] = useState(false);
+
+  const toggleShare = () => {
+    resetCopySuccess();
+    setIsShareOpen(!isShareOpen);
+  };
+
+  const copyPostUrl = () => {
+    const postUrl = `${window.location.origin}/p/${imageName}`;
+    navigator.clipboard
+      .writeText(postUrl)
+      .then(() => {
+        setCopySuccess(true);
+        setTimeout(() => {
+          setCopySuccess(false);
+        }, 1000);
+      })
+      .catch((error) => {
+        console.error("Error al copiar la URL:", error);
+        setCopySuccess(false);
+      });
+  };
+
+  const resetCopySuccess = () => {
+    setCopySuccess(false);
+  };
+
+  // NOTA: corregir hora
   console.log(post.created_at);
   return (
     <article>
@@ -38,13 +68,23 @@ export const Post = ({ post }) => {
       <div className="post-icons-below">
         <img src={heartIcon} alt="Icono de Like"></img>
         <img src={messageIcon} alt="Icono de Mensajes"></img>
-        <img src={sendIcon} alt="Icono de Compartir"></img>
+        <img
+          src={sendIcon}
+          alt="Icono de Compartir"
+          onClick={toggleShare}
+        ></img>
+        {isShareOpen && (
+          <div>
+            <button onClick={copyPostUrl}>Compartir</button>
+          </div>
+        )}
         <img
           src={bookmarkIcon}
           alt="Icono de Marcadores"
           className="post-icons-bookmark"
         ></img>
       </div>
+      {copySuccess && <p className="url-copy">¡URL copiada correctamente!</p>}
       <p className="likes-count">Likes = {post?.likes}</p>
       {/* NOTA: si post_text es muy largo, rompe la posición entre img y resto */}
       {post?.post_text && (
